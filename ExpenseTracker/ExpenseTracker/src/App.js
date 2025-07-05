ca7875b7f086164a1f48d603917725c28528d671
nandakrishna21 <nandakrishna4283@gmail.com>
2025-07-05 20:41:07 +0530

    Add ExpenseTracker project files

---
+++ ExpenseTracker/ExpenseTracker/src/App.js
@@ -0,0 +1,12 @@
+
+import Balance from './components/Balance';
+
+function App() {
+  return (
+    <div className="bg-slate-300 h-screen">
+      <Balance />
+    </div>
+  );
+}
+
+export default App;
